92b63f605cbb810c52611d127f847a16fd6f0ee2
lipinra <lipinr@ya.ru>
2023-09-21 16:47:32 +0300

    добавил apache superset

--- template/backup.sh
+++ template/backup.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # архивируем с указанимем даты в названии файла
-tar -czvf /root/metabase/backup/backup_`date +%Y-%m-%d"_"%H_%M_%S`.tar /root/metabase/pgadmin-data /root/metabase/nginx /etc/letsencrypt /root/metabase/docker-compose.yml /root/metabase/db_password.txt /root/metabase/db_user.txt
+tar -czvf /root/metabase/backup/backup_`date +%Y-%m-%d"_"%H_%M_%S`.tar /root/metabase/metabase-data /root/metabase/pgadmin-data /root/metabase/nginx /etc/letsencrypt /root/metabase/docker-compose.yml /root/metabase/db_password.txt /root/metabase/db_user.txt /root/metabase/pgadmin_password.txt
 # бекапим postgres
 docker-compose --file /root/metabase/docker-compose.yml run postgres pg_dumpall -c -U postgres > /root/metabase/backup/dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
+# бекапим superset
+docker cp superset:/var/lib/superset/superset.db /root/metabase/backup/superset_`date +%Y-%m-%d"_"%H_%M_%S`.db
 # удаляем все файлы старше 7 дней
 find /root/metabase/backup/ -type f -mtime +7 -delete
 
